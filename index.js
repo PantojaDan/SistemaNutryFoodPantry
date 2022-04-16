d7ea6dbd0c1d48e42079851710a15283161ba18e
Daniel <biyuyisdan@gmail.com>
2022-04-16 12:32:22 -0500

    Quite la animacion de loader

--- index.js
+++
@@ -1,9 +0,0 @@
-const capa = document.querySelector('.capa');
-const iconLoad = document.querySelector('.icon-load');
-const ingresar = document.querySelector('#ingresar');
-
-ingresar.addEventListener('click',()=>{
-    capa.classList.remove('hide');
-    capa.classList.add('show');
-    iconLoad.classList.remove('paused');
-});
