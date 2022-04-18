854c2b7ce813a0ce5feefd2b761150f73919350f
Daniel <biyuyisdan@gmail.com>
2022-04-18 09:53:51 -0500

    Se agrego un modal

--- dashboard.js
+++ dashboard.js
@@ -1,6 +1,9 @@
 const menuDashboard = document.querySelector('.menu-dashboard');
 const menuHamburguer = document.querySelector('.menu-hamburguer-dash');
 
+const agregarLista = document.querySelector('.agregar-lista');
+const containerAgregar = document.querySelector('.container-agregar-inv');
+
 menuHamburguer.addEventListener('click',()=>{
     if(menuDashboard.classList.contains('hide-menu')){
         menuDashboard.classList.remove('hide-menu');
@@ -9,4 +12,10 @@ menuHamburguer.addEventListener('click',()=>{
         menuDashboard.classList.remove('show-menu');
         menuDashboard.classList.add('hide-menu');
     }
+});
+
+
+agregarLista.addEventListener('click',()=>{
+    containerAgregar.classList.remove('ocultar-agregar-inv');
+    containerAgregar.classList.add('mostrar-agregar-inv');
 });
